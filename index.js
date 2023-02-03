5fd775528c432a789d50a28e7985df8edd7723e1
Jayro Gómez <jaxjayro@gmail.com>
2023-02-03 16:07:15 -0600

    VideoChat Server

--- index.js
+++ index.js
@@ -4,23 +4,40 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const cors = require('cors')
-const { route } = require('./routes')
 
 //Setting the socket with all origins allowed
 const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST'] 
-    }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 })
 
 app.use(cors())
 
- //Setting routes
-const routes = require('./routes/index')
+//Setting routes
+const routes = require('./routes')
 app.use('/', routes)
 
-//Setting port
+//socket conecction
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id)
+
+  //Handelers 
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('call ended')
+  })
+
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name })
+  })
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to.emit('callAccepted'), data.signal)
+  })
+})
+
+//Serving port
 const port = process.env.PORT || 4000
 server.listen(port, () => console.log(`Server Lister on port ${port}`))
 
